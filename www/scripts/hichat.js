7a958b68e3290897ed0172c95356981b865fd5ac
Allen <allen.alben@gmail.com>
2021-05-24 00:38:22 +0530

    Final Commit

--- www/scripts/hichat.js
+++ www/scripts/hichat.js
@@ -1,46 +1,46 @@
-window.onload = function() {
+window.onload = function () {
     var hichat = new HiChat();
     hichat.init();
 };
-var HiChat = function() {
+var HiChat = function () {
     this.socket = null;
 };
 HiChat.prototype = {
-    init: function() {
+    init: function () {
         var that = this;
         this.socket = io.connect();
-        this.socket.on('connect', function() {
-            document.getElementById('info').textContent = 'get yourself a nickname :)';
+        this.socket.on('connect', function () {
+            document.getElementById('info').textContent = 'ENTER A NICKNAME FOR YOURSELF';
             document.getElementById('nickWrapper').style.display = 'block';
             document.getElementById('nicknameInput').focus();
         });
-        this.socket.on('nickExisted', function() {
+        this.socket.on('nickExisted', function () {
             document.getElementById('info').textContent = '!nickname is taken, choose another pls';
         });
-        this.socket.on('loginSuccess', function() {
-            document.title = 'hichat | ' + document.getElementById('nicknameInput').value;
+        this.socket.on('loginSuccess', function () {
+            document.title = 'WhatsChat | ' + document.getElementById('nicknameInput').value;
             document.getElementById('loginWrapper').style.display = 'none';
             document.getElementById('messageInput').focus();
         });
-        this.socket.on('error', function(err) {
+        this.socket.on('error', function (err) {
             if (document.getElementById('loginWrapper').style.display == 'none') {
                 document.getElementById('status').textContent = '!fail to connect :(';
             } else {
                 document.getElementById('info').textContent = '!fail to connect :(';
             }
         });
-        this.socket.on('system', function(nickName, userCount, type) {
+        this.socket.on('system', function (nickName, userCount, type) {
             var msg = nickName + (type == 'login' ? ' joined' : ' left');
             that._displayNewMsg('system ', msg, 'red');
             document.getElementById('status').textContent = userCount + (userCount > 1 ? ' users' : ' user') + ' online';
         });
-        this.socket.on('newMsg', function(user, msg, color) {
+        this.socket.on('newMsg', function (user, msg, color) {
             that._displayNewMsg(user, msg, color);
         });
-        this.socket.on('newImg', function(user, img, color) {
+        this.socket.on('newImg', function (user, img, color) {
             that._displayImage(user, img, color);
         });
-        document.getElementById('loginBtn').addEventListener('click', function() {
+        document.getElementById('loginBtn').addEventListener('click', function () {
             var nickName = document.getElementById('nicknameInput').value;
             if (nickName.trim().length != 0) {
                 that.socket.emit('login', nickName);
@@ -48,7 +48,7 @@ HiChat.prototype = {
                 document.getElementById('nicknameInput').focus();
             };
         }, false);
-        document.getElementById('nicknameInput').addEventListener('keyup', function(e) {
+        document.getElementById('nicknameInput').addEventListener('keyup', function (e) {
             if (e.keyCode == 13) {
                 var nickName = document.getElementById('nicknameInput').value;
                 if (nickName.trim().length != 0) {
@@ -56,7 +56,7 @@ HiChat.prototype = {
                 };
             };
         }, false);
-        document.getElementById('sendBtn').addEventListener('click', function() {
+        document.getElementById('sendBtn').addEventListener('click', function () {
             var messageInput = document.getElementById('messageInput'),
                 msg = messageInput.value,
                 color = document.getElementById('colorStyle').value;
@@ -68,7 +68,7 @@ HiChat.prototype = {
                 return;
             };
         }, false);
-        document.getElementById('messageInput').addEventListener('keyup', function(e) {
+        document.getElementById('messageInput').addEventListener('keyup', function (e) {
             var messageInput = document.getElementById('messageInput'),
                 msg = messageInput.value,
                 color = document.getElementById('colorStyle').value;
@@ -78,10 +78,10 @@ HiChat.prototype = {
                 that._displayNewMsg('me', msg, color);
             };
         }, false);
-        document.getElementById('clearBtn').addEventListener('click', function() {
+        document.getElementById('clearBtn').addEventListener('click', function () {
             document.getElementById('historyMsg').innerHTML = '';
         }, false);
-        document.getElementById('sendImage').addEventListener('change', function() {
+        document.getElementById('sendImage').addEventListener('change', function () {
             if (this.files.length != 0) {
                 var file = this.files[0],
                     reader = new FileReader(),
@@ -91,7 +91,7 @@ HiChat.prototype = {
                     this.value = '';
                     return;
                 };
-                reader.onload = function(e) {
+                reader.onload = function (e) {
                     this.value = '';
                     that.socket.emit('img', e.target.result, color);
                     that._displayImage('me', e.target.result, color);
@@ -100,18 +100,18 @@ HiChat.prototype = {
             };
         }, false);
         this._initialEmoji();
-        document.getElementById('emoji').addEventListener('click', function(e) {
+        document.getElementById('emoji').addEventListener('click', function (e) {
             var emojiwrapper = document.getElementById('emojiWrapper');
             emojiwrapper.style.display = 'block';
             e.stopPropagation();
         }, false);
-        document.body.addEventListener('click', function(e) {
+        document.body.addEventListener('click', function (e) {
             var emojiwrapper = document.getElementById('emojiWrapper');
             if (e.target != emojiwrapper) {
                 emojiwrapper.style.display = 'none';
             };
         });
-        document.getElementById('emojiWrapper').addEventListener('click', function(e) {
+        document.getElementById('emojiWrapper').addEventListener('click', function (e) {
             var target = e.target;
             if (target.nodeName.toLowerCase() == 'img') {
                 var messageInput = document.getElementById('messageInput');
@@ -120,7 +120,7 @@ HiChat.prototype = {
             };
         }, false);
     },
-    _initialEmoji: function() {
+    _initialEmoji: function () {
         var emojiContainer = document.getElementById('emojiWrapper'),
             docFragment = document.createDocumentFragment();
         for (var i = 69; i > 0; i--) {
@@ -131,7 +131,7 @@ HiChat.prototype = {
         };
         emojiContainer.appendChild(docFragment);
     },
-    _displayNewMsg: function(user, msg, color) {
+    _displayNewMsg: function (user, msg, color) {
         var container = document.getElementById('historyMsg'),
             msgToDisplay = document.createElement('p'),
             date = new Date().toTimeString().substr(0, 8),
@@ -142,7 +142,7 @@ HiChat.prototype = {
         container.appendChild(msgToDisplay);
         container.scrollTop = container.scrollHeight;
     },
-    _displayImage: function(user, imgData, color) {
+    _displayImage: function (user, imgData, color) {
         var container = document.getElementById('historyMsg'),
             msgToDisplay = document.createElement('p'),
             date = new Date().toTimeString().substr(0, 8);
@@ -151,7 +151,7 @@ HiChat.prototype = {
         container.appendChild(msgToDisplay);
         container.scrollTop = container.scrollHeight;
     },
-    _showEmoji: function(msg) {
+    _showEmoji: function (msg) {
         var match, result = msg,
             reg = /\[emoji:\d+\]/g,
             emojiIndex,
